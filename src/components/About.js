c2a4098054d59047ff010f9853dd2697d4fda2b5
AJ Ferron <alvn.frrn@gmail.com>
2020-04-19 02:52:09 -0400

    Create About page

---
+++ src/components/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function About() {
+    return (
+        // Works like a ghost element, doesn't show in DOM
+        <React.Fragment>
+            <header>
+                <h1>About</h1>
+                <button>
+                    <Link to="/">
+                        <img src="./circle-x.svg" alt="Back"/>
+                    </Link>
+                </button>
+            </header>
+            <p>
+                A simple To Do app made with React.js <br/><br/>
+                <a href="https://www.youtube.com/watch?v=sBws8MSXN7A"> React JS Crash Course </a>
+            </p>
+        </React.Fragment>
+    )
+}
+
+export default About;
